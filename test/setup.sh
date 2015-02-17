6fab6371935d2b5f9b21b5a97790e2265024a31c
Michael Bridgen <mikeb@squaremobius.net>
2015-02-17 14:24:47 -0800

    Use weave remotely in smoke tests instead of via SSH

--- test/setup.sh
+++ test/setup.sh
@@ -14,10 +14,9 @@ for HOST in $HOSTS; do
     docker_on $HOST load -i /var/tmp/weave.tar
     docker_on $HOST load -i /var/tmp/weavedns.tar
     docker_on $HOST load -i /var/tmp/weavetools.tar
-    run_on $HOST mkdir -p `dirname $WEAVE`
-    cat ../weave         | run_on $HOST sh -c "cat > $WEAVE"
+    run_on $HOST mkdir -p bin
+    cp ../weave ./
     cat ../bin/docker-ns | run_on $HOST sh -c "cat > $DOCKER_NS"
-    run_on $HOST chmod a+x $WEAVE
     run_on $HOST chmod a+x $DOCKER_NS
     run_on $HOST sudo service docker restart
 done
