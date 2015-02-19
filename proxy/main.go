c2f21aa8b065f7ac963cf9f00f7e38cbd5e40521
Michael Bridgen <mikeb@squaremobius.net>
2015-02-18 16:58:43 -0800

    Add interceptors for create and start
    The pre-create hook is a no-op; the post-start hook tells weave to
    attach an IP to the container if it's been given a WEAVE_CIDR env
    entry.
    
    The proxy is included in the weavetools, and run as `weave
    launch-proxy`. One subtlety is that the proc filesystem that is
    mounted in the proxy image should be that of the host, not that given
    the script. I've hard-wired it to /proc, but it would probably need to
    be given as another argument or env entry; or perhaps better, it could
    be chosen depending on whether it's run locally or remotely.

--- proxy/main.go
+++ proxy/main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
 	. "github.com/zettio/weave/common"
 	"io"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os/exec"
+	"regexp"
+	"strings"
 )
 
 const (
+	WEAVE = "./weave"
 	RAW_STREAM = "application/vnd.docker.raw-stream"
 )
 
@@ -72,18 +79,118 @@ func main() {
 	}
 }
 
+// ---- Proxy implementation
+
+func containerInfo(proxy *Proxy, containerId string) (body map[string]interface{}, err error) {
+	body = nil
+	client := &http.Client{
+		Transport: proxy,
+	}
+	if res, err := client.Get("http://localhost/v1.16/containers/" + containerId + "/json"); err == nil {
+		if bs, err := ioutil.ReadAll(res.Body); err == nil {
+			err = json.Unmarshal(bs, &body)
+		} else {
+			Warning.Print("Could not parse response from docker", err)
+		}
+	} else {
+		Warning.Print("Error fetching container info from docker", err)
+	}
+	return
+}
+
+func isCreate(r *http.Request) bool {
+	ok, err := regexp.MatchString("^/v[0-9\\.]*/containers/create$", r.URL.Path)
+	return err == nil && ok
+}
+
+func isStart(r *http.Request) bool {
+	ok, err := regexp.MatchString("^/v[0-9\\.]*/containers/[^/]*/start$", r.URL.Path)
+	return err == nil && ok
+}
+
+func containerFromPath(path string) string {
+	if subs := regexp.MustCompile("^/v[0-9\\.]*/containers/([^/]*)/.*").FindStringSubmatch(path); subs != nil {
+		return subs[1]
+	}
+	return ""
+}
+
+func callWeave(args ...string) ([]byte, error) {
+	Debug.Print("Calling weave", args)
+	cmd := exec.Command(WEAVE, args...)
+	cmd.Env = []string{"PROCFS=/hostproc", "PATH=/usr/sbin:/usr/bin:/sbin:/bin"}
+	out, err := cmd.CombinedOutput()
+	return out, err
+}
+
+func weaveAddrFromConfig(config map[string]interface{}) string {
+	if entries, ok := config["Env"].([]interface{}); ok {
+		for _, e := range entries {
+			entry := e.(string)
+			if strings.Index(entry, "WEAVE_CIDR=") == 0 {
+				return entry[11:]
+			}
+		}
+	} else {
+		Warning.Print("Unexpected format for config", config)
+	}
+	return ""
+}
+
+func (proxy *Proxy) InterceptRequest(r *http.Request) (*http.Request, error) {
+	body := r.Body
+	if isCreate(r) {
+		var config map[string]interface{}
+		bs, _ := ioutil.ReadAll(body)
+		body = ioutil.NopCloser(bytes.NewReader(bs))
+		if err := json.Unmarshal(bs, &config); err == nil {
+			if cidr := weaveAddrFromConfig(config); cidr != "" {
+				Info.Printf("Creating container with CIDR %s", cidr)
+			}
+		} else {
+			Warning.Print("Unable to decode CREATE body: ", err)
+		}
+	}
+	req, err := http.NewRequest(r.Method, r.URL.Path, body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header = r.Header
+	req.URL.RawQuery = r.URL.RawQuery
+	return req, nil
+}
+
+func (proxy *Proxy) InterceptResponse(req *http.Request, res *http.Response) *http.Response {
+	if isStart(req) {
+		if containerId := containerFromPath(req.URL.Path); containerId != "" {
+			if info, err := containerInfo(proxy, containerId); err == nil {
+				Debug.Print(info)
+				if cidr := weaveAddrFromConfig(info["Config"].(map[string]interface{})); cidr != "" {
+					if out, err := callWeave("--local", "attach", cidr, containerId); err != nil {
+						Warning.Print("Calling weave failed:", err, string(out))
+					}
+				} else {
+					Debug.Print("No Weave CIDR, ignoring")
+				}
+			} else {
+				Warning.Print("Unable to fetch container info: ", err)
+			}
+		} else {
+			Error.Fatal("Cound not extract container ID from path")
+		}
+	}
+	return res
+}
+
 func (proxy *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	Info.Printf("%s %s", r.Method, r.URL)
-	// These just to fool the HTTP code into doing my bidding
-	req, err := http.NewRequest(r.Method, r.URL.Path, r.Body)
+	req, err := proxy.InterceptRequest(r)
 	if err != nil {
 		http.Error(w, "Unable to create proxied request", http.StatusInternalServerError)
 		Warning.Print(err)
 		return
 	}
 	req.Close = false
-	req.Header = r.Header
-	req.URL.RawQuery = r.URL.RawQuery
 
 	conn, err := proxy.Dial()
 	if err != nil {
@@ -95,6 +202,7 @@ func (proxy *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	defer client.Close()
 
 	resp, _ := client.Do(req)
+	resp = proxy.InterceptResponse(req, resp)
 
 	hdr := w.Header()
 	for k, vs := range resp.Header {
@@ -149,4 +257,15 @@ func (proxy *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			Warning.Print(err)
 		}
 	}
+}
+
+func (proxy *Proxy) RoundTrip(req *http.Request) (*http.Response, error) {
+	t := &http.Transport{
+		Proxy: nil,
+		Dial: func(string, string) (net.Conn, error) {
+			return proxy.Dial()
+		},
+	}
+	res, err := t.RoundTrip(req)
+	return res, err
 }
